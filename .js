6b1ccddc0a548b59fcf80680bd15bf50849adf73
EmeraldNZ <ahyuancy@gmail.com>
2018-12-19 10:22:09 +1300

    Update .js

--- .js
+++ .js
@@ -1,8 +1,8 @@
 function splitify(str) {
- let newStr = str.replace(/\W/g, ' ');
+  let newStr = str.replace(/\W/g, ' ');
   
   return newStr.split(" ");
  
- return str.split(/\W/); // Simply split the string to create a new array of words.
+ // return str.split(/\W/); // Or simply split the string to create a new array of words since strings are immutable
 }
 splitify("Hello World,I-am code");
