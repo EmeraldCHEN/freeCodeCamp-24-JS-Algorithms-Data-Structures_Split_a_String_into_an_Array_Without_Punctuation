08e2ecc529d4e98555b34a9cbf43c269b6ab54c4
EmeraldNZ <ahyuancy@gmail.com>
2018-12-19 10:27:39 +1300

    Update .js

--- .js
+++ .js
@@ -1,8 +1,16 @@
 function splitify(str) {
+  
   let newStr = str.replace(/\W/g, ' ');
   
   return newStr.split(" ");
  
- // return str.split(/\W/); // Or simply split the string to create a new array of words since strings are immutable
+  // return str.split(/\W/); // Or simply split the string to create a new array of words since strings are immutable
 }
-splitify("Hello World,I-am code");
+
+splitify("Hello World,I-am coding"); // Returns ["Hello", "World", "I", "am", "coding"]
+
+
+// Retrieved from
+// https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/functional-programming/split-a-string-into-an-array-using-the-split-method/
+
+
